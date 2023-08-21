7e0f48ab20abead55ede011865f0fe89d5bc91b3
gino885 <ginozxcv885@gmail.com>
2023-08-21 20:36:12 +0800

    商品功能 - sort

--- springboot-mall/src/main/java/com/gino/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/gino/springbootmall/controller/ProductController.java
@@ -22,12 +22,19 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            //查詢條件filtering
            @RequestParam(required = false) ProductCategory category,
-           @RequestParam(required = false) String search
+           @RequestParam(required = false) String search,
+
+           //排序sorting
+           @RequestParam(defaultValue = "created_date") String orderBy,
+           @RequestParam(defaultValue = "desc")String sort
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setSort(sort);
+        productQueryParams.setOrderBy(orderBy);
         List<Product> productList = productService.getProducts(productQueryParams);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
--- springboot-mall/src/main/java/com/gino/springbootmall/dto/ProductQueryParams.java
+++ springboot-mall/src/main/java/com/gino/springbootmall/dto/ProductQueryParams.java
@@ -5,6 +5,8 @@ import com.gino.springbootmall.constant.ProductCategory;
 public class ProductQueryParams {
     private ProductCategory category;
     private String search;
+    private String orderBy;
+    private String sort;
 
     public ProductCategory getCategory() {
         return category;
@@ -20,5 +22,21 @@ public class ProductQueryParams {
 
     public void setSearch(String search) {
         this.search = search;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
     }
 }
